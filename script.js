3d97c3be68daa88812450b3a94026c3124dde413
Cynthia <cynthia.obia@gmail.com>
2023-07-17 20:19:36 -0500

    determine winning combos"

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 // combinations
 
-const arr = ['', '', '', '', '', '', '', '', ''];
+const arr = [0, 1, 2, 3, 4, 5, 6, 7, 0];
 
 // practice inputs
 // there are 8 winning combinations in tic tac toe
 
 // WINNING COMBINATIONS
+// HORIZONTALLY:
+//  0 1 2
+//  3 4 5
+//  6 7 8
+// VERTICALLY:
+//  0 3 6
+//  1 4 7
+//  2 5 8
+// DIAGONALLY:
+//  0 4 8
+//  2 4 6
